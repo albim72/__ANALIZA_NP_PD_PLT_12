7466b11f459548af2a57428c265bf4e621d3fd8e
albim72 <marcin.albiniak72@gmail.com>
2024-12-09 12:27:25 +0100

    Update main.py

--- DZIEN_1/moje_moduly/main.py
+++ DZIEN_1/moje_moduly/main.py
@@ -2,9 +2,14 @@
 # import dane as dn
 from dane import nr_filii as nf
 from dane import book as bk
+from funkcje.mojefunkcje import czytaj_liste,czytaj_slownik
 
 #CTRL + D -> duplikacja liniibloku tekstu
 #CTRL + / -> szybkie komentowane linii/bloku
 print("___________________ DANE ___________________")
 print(nf)
 print(bk)
+
+print("___________________ FUNKCJE ___________________")
+czytaj_liste(nf)
+czytaj_slownik(bk)
