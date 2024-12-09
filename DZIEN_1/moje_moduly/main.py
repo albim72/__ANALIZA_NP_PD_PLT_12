f77b46142da17835b408c2ce43e453e6e74bbc16
albim72 <marcin.albiniak72@gmail.com>
2024-12-09 12:16:51 +0100

    Create main.py

---
+++ DZIEN_1/moje_moduly/main.py
@@ -0,0 +1,10 @@
+# import dane
+# import dane as dn
+from dane import nr_filii as nf
+from dane import book as bk
+
+#CTRL + D -> duplikacja liniibloku tekstu
+#CTRL + / -> szybkie komentowane linii/bloku
+print("___________________ DANE ___________________")
+print(nf)
+print(bk)
