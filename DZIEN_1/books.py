8fb7e8a44a396eeab0c5656b2ac00afa7910d69a
albim72 <marcin.albiniak72@gmail.com>
2024-12-09 14:03:21 +0100

    Update books.py

--- DZIEN_1/books.py
+++ DZIEN_1/books.py
@@ -19,13 +19,37 @@ class Book:
     def __call__(self,messsage):
         print(f"Ważna wiadmość: {messsage} -> dotyczy ksiązki: {self.idbook} {self.tytul}")
 
-
     def create_book(self):
         print("Utworzono obiekt klasy Book!(nową książkę)")
 
+    def get_cena(self):
+        return self.cena
+
+    def set_cena(self,nowacena):
+        self.cena = nowacena
+
+    @property
+    def oprawa(self):
+        return self._oprawa
+
+    @oprawa.setter
+    def oprawa(self,nowaoprawa):
+        self._oprawa = nowaoprawa
 
 
 bk = Book(26,"Moje Góry","Jan Byrcyn",45)
 print(bk)
 bk("promocja przesunięta na dzień 15.01.2025!")
+# bk.cena = 48
+# print(f"cena po zmianie: {bk.cena}")
+
+bk.set_cena(48)
+print(f"cena po zmianie: {bk.get_cena()}")
+print(f"oprawa przed zmianą: {bk.oprawa}")
+bk.oprawa = "twarda"
+print(f"oprawa po zmianie: {bk.oprawa}")
+
+
+
+
 
